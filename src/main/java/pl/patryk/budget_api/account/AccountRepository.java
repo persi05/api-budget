ab7e896ee3550648e4ae62e70076f7597a59fb4c
Patryk Persak <abogusiac@gmail.com>
2026-06-02 18:19:14 +0200

    Add account repository

---
+++ src/main/java/pl/patryk/budget_api/account/AccountRepository.java
@@ -0,0 +1,6 @@
+package pl.patryk.budget_api.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
