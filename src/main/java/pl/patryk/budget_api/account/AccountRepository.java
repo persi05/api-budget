d37cc4dcb9cf9ec42e98fb7d58a223ec7c77d3ec
Patryk Persak <abogusiac@gmail.com>
2026-06-05 11:16:07 +0200

    Add check if account name is exist

--- src/main/java/pl/patryk/budget_api/account/AccountRepository.java
+++ src/main/java/pl/patryk/budget_api/account/AccountRepository.java
@@ -3,4 +3,6 @@ package pl.patryk.budget_api.account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByNameIgnoreCase(String name);
 }
